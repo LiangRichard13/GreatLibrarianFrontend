63ee533e8b964f89f063622d6410bd7047511681
LiXiang <945794759@qq.com>
2024-02-29 16:55:34 +0800

    修改配置文件的执行顺序bug及接收choose参数的数据类型~

--- Backend/App/apis/testProjectUser.py
+++ Backend/App/apis/testProjectUser.py
@@ -22,14 +22,14 @@ class TestProjectUserCRUD(Resource):
     # 查询【参数：choose查询选择（1:通过实验id找协作者;2:通过协作者id找实验）】
     def get(self):
         # 通过实验id找协作者【参数:TPid实验ID】
-        if request.args['choose'] == 1:
+        if request.args['choose'] == '1':
             tPUList = TestProjectUser.query.filter(TestProjectUser.TPid == request.args['TPid'])
             collaborators = []
             for tPU in tPUList:
                 collaborators.append({'id': tPU.uid, 'name': User.query.filter(User.user_id == tPU.uid)[0].user_name})
             return jsonify({'success': True, 'data': collaborators})  # 返回协作者用户ID列表
         # 通过协作者id找实验【参数:uid协作者用户ID】
-        if request.args['choose'] == 2:
+        if request.args['choose'] == '2':
             tPUList = TestProjectUser.query.filter(TestProjectUser.TPid == request.args['uid'])
             testProjects = []
             for tPU in tPUList:

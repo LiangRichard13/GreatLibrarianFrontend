0397a94b89f157cd2f212291b4119357eb878abc
RichardLiang13 <1760268004@qq.com>
2024-03-07 14:24:03 +0800

    加入了项目配置修改功能和实验列表标签页控制，能够在实验列表通过标签页进行不同状态实验列表的跳转

--- vue-frontend/src/components/CollaborateNavigation.vue
+++ vue-frontend/src/components/CollaborateNavigation.vue
@@ -12,21 +12,36 @@
   </el-container>
 </template>
 
-
 <script>
 export default {
   name: "CollaborateNavigation",
- data() {
-      return {
-        activeIndex: '1',
-      };
-},
-methods: {
-      handleSelect(key) {
-         if (this.$route.path !== key)
-           this.$router.push(key);
+  data() {
+    return {
+      activeIndex: '', // 初始化为空字符串或者根据你的路由设置一个默认值
+    };
+  },
+  mounted() {
+    this.setActiveIndex(); // 当组件挂载时设置activeIndex
+  },
+  watch: {
+    // 监听路由变化以更新菜单激活状态
+    '$route.path': {
+      immediate: true, // 立即触发，确保刷新页面时也能获取正确的状态
+      handler() {
+        this.setActiveIndex();
       }
     }
+  },
+  methods: {
+    handleSelect(key){
+      if (this.$route.path !== key)
+        this.$router.push(key);
+    },
+    setActiveIndex() {
+      // 将activeIndex设置为当前路由的路径
+      this.activeIndex = this.$route.path;
+    }
+  }
 }
 </script>
 

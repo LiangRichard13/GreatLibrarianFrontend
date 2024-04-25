d227c332e14d6389aa95df842a42cf13137a06a9
RichardLiang13 <1760268004@qq.com>
2024-04-25 10:40:02 +0800

    更新

--- vue-frontend/src/views/Experiment/ExperimentList.vue
+++ vue-frontend/src/views/Experiment/ExperimentList.vue
@@ -375,11 +375,11 @@
                         </el-select>
                     </el-form-item>
 
-                    <el-form-item label="指定测试类型">
+                    <!-- <el-form-item label="指定测试类型">
                         <el-switch v-model="newExpirement.type" active-color="#13ce66" inactive-color="#409EFF" active-value="1"
                             inactive-value="0" active-text="幻觉测试" inactive-text="基础测试">
                         </el-switch>
-                    </el-form-item>
+                    </el-form-item> -->
 
                     <!-- 进行测试配置文件的编辑 -->
                     <!-- <el-form-item>
@@ -557,7 +557,8 @@ export default {
             editDialog: false,
             downloader: false,
             experimentList: [],
-            newExpirement: { name: '', AK1: '', AK2: '', DS: '',type:0},
+            // newExpirement: { name: '', AK1: '', AK2: '', DS: '',type:0},
+            newExpirement: { name: '', AK1: '', AK2: '', DS: ''},
             expList: [], // 待测试列表数据
             proceeding: [],//正在测试列表
             reviewList: [], // 待审核列表数据
